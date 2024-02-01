98dddd96f1d7501ebffc540bfd20cfecdeef6002
hussein1997323 <husseinothman660@gmail.com>
2024-01-31 22:48:59 -0300

    criação

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '9mze88',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
